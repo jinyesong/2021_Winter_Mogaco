7f30b03b971bc3e9c5cb3f9cb95726deb091b26b
Yejin Song <songyejin2000@gmail.com>
2022-03-01 17:09:48 +0900

    #6.4

--- react-mogaco/src/App.js
+++ react-mogaco/src/App.js
@@ -1,36 +1,25 @@
 
 import {useState, useEffect} from "react";
 
+function Hello(){
+  useEffect(()=>{
+    console.log("I'm created");
+    return ()=> console.log("destroyed"); //컴포넌트가 destroyed될 때도 함수 설정 가능, useEffect의 return
+  }, []);
+  return(
+    <h1>Hello</h1>
+  );
+}
+
 function App() {
-  const [counter, setCount] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  console.log("계속실행");
-  const runOnlyOnce = ()=>{
-    console.log("한번실행");
-  }
-
-  useEffect(runOnlyOnce, []); //맨 처음 한번만 실행
-
-  useEffect(()=>{ 
-    if(keyword !== ""){
-      console.log("search for", keyword);
-    }
-  }, [keyword]); //keyword의 변화가 있을 때만 실행
-
+  const [showing, setShowing] = useState(false);
   const onClick = ()=>{
-    setCount(current => current +1);
+    setShowing(current => !current);
   }
-
-  const onChange = (event)=>{
-    setKeyword(event.target.value);
-  }
-
   return (
     <div className="App">
-      <input onChange={onChange} type="text" placeholder="Search here" />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>counter</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
