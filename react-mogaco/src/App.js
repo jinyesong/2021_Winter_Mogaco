361821bcefe53d0140d2a0c2f18ea37e48aa69f0
Yejin Song <songyejin2000@gmail.com>
2022-03-01 16:01:56 +0900

    #6.2

--- react-mogaco/src/App.js
+++ react-mogaco/src/App.js
@@ -1,19 +1,34 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+
 import {useState, useEffect} from "react";
 
 function App() {
   const [counter, setCount] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   console.log("계속실행");
   const runOnlyOnce = ()=>{
     console.log("한번실행");
   }
-  useEffect(runOnlyOnce, []);
+
+  useEffect(runOnlyOnce, []); //맨 처음 한번만 실행
+
+  useEffect(()=>{ 
+    if(keyword !== ""){
+      console.log("search for", keyword);
+    }
+  }, [keyword]); //keyword의 변화가 있을 때만 실행
+
   const onClick = ()=>{
     setCount(current => current +1);
   }
+
+  const onChange = (event)=>{
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="App">
+      <input onChange={onChange} type="text" placeholder="Search here" />
       <h1>{counter}</h1>
       <button onClick={onClick}>counter</button>
     </div>
