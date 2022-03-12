81136154dbffc7b7c7dbcc75a7fc55116f06de6d
Yejin Song <songyejin2000@gmail.com>
2022-03-12 21:11:09 +0900

    #7.3

--- react-mogaco/src/App.js
+++ react-mogaco/src/App.js
@@ -3,24 +3,32 @@ import {useState, useEffect} from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async()=>{
+    const json = await (await fetch("list_movies")).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
   useEffect(()=>{
-    fetch("coinpaprika api").then(response=>response.json()
-    ).then(json=>{
-      setCoins(json);
-      setLoading(false);
-    });
+    getMovies();
+  //   fetch("list_movies").then(response => response.json().then(json => {
+  //   setMovies(json.data.movies)
+  //   setLoading(false)
+  // }));
   }, []);
+  console.log(movies);
   return (
     <div className="App">
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1> {/*({loading ? "" : coins.length})*/}
-      {loading?<strong>Loading...</strong>:<select>
-        {coins.map((coin) => <option>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</option>)}
-      </select>}
+      {loading ? <h1>Loading...</h1> : <div>{movies.map(movie=><div key={movie.id}>
+        <img src={movie.medium_cover_image} />
+        <h2>{movie.title}</h2>
+        <p>{movie.summary}</p>
+        <ul>
+          {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+        </ul>
+        </div>)}</div>}
     </div>
   );
 }
 
 export default App;
-
-// code challenge: coin의 목록을 select로 선택하게 해서 USD가 얼마의 비트코인으로 환산되는지를 알려주는 웹페이지 제작해보기
