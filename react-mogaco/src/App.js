c5e7f8685ea2a3fed5da634ccace363d4e60abcb
Yejin Song <songyejin2000@gmail.com>
2022-03-14 21:56:20 +0900

    #7.4

--- react-mogaco/src/App.js
+++ react-mogaco/src/App.js
@@ -1,34 +1,5 @@
-
-import {useState, useEffect} from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async()=>{
-    const json = await (await fetch("list_movies")).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  }
-  useEffect(()=>{
-    getMovies();
-  //   fetch("list_movies").then(response => response.json().then(json => {
-  //   setMovies(json.data.movies)
-  //   setLoading(false)
-  // }));
-  }, []);
-  console.log(movies);
-  return (
-    <div className="App">
-      {loading ? <h1>Loading...</h1> : <div>{movies.map(movie=><div key={movie.id}>
-        <img src={movie.medium_cover_image} />
-        <h2>{movie.title}</h2>
-        <p>{movie.summary}</p>
-        <ul>
-          {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
-        </ul>
-        </div>)}</div>}
-    </div>
-  );
+  return null;
 }
 
 export default App;
