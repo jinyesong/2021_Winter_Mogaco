c5120b1c1553c0c7584cc01e74ed09fb44afc87d
Yejin Song <songyejin2000@gmail.com>
2022-03-19 20:40:58 +0900

    #7.5

--- react-mogaco/src/App.js
+++ react-mogaco/src/App.js
@@ -1,5 +1,20 @@
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+//Link: 브라우저 새로고침 없이 다른 페이지로 이동시켜주는 컴포넌트
+} from "react-router-dom";
+import Home from "./route/Home";
+import Detail from "./route/Detail";
+
 function App() {
-  return null;
+  return (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie" element={<Detail />} />
+    </Routes>
+  </Router>);
 }
 
 export default App;
--- react-mogaco/src/components/Movie.js
+++ react-mogaco/src/components/Movie.js
@@ -1,10 +1,11 @@
 import propTypes from "prop-types";
+import {Link} from "react-router-dom";
 
 function Movie({ coverImg, title, summary, genres }) {
     return (
         <div>
             <img src={coverImg} alt="title" />
-            <h2>{title}</h2>
+            <Link to="/movie">{title}</Link>
             <p>{summary}</p>
             <ul>
                 {genres.map(genre => <li key={genre}>{genre}</li>)}
